0eb269d34a29e7233e1453997aaf34eb0a8cc763
Zach Lehman <zalehman182@stevenscollege.edu>
2019-06-16 13:59:57 -0400

    Add Footer to each page and added js function and tweaked each page

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ function addMoreMerch() {
   }
   count += 1;
 };
+
+// function todayDate() {
+//   const date = new Date();
+//   const printedDate = new Array(7);
+//   printedDate[0] = "Sunday 11:00am-6:00pm"
+//   printedDate[1] = "Monday 10:00am-9:00pm"
+//   printedDate[2] = "Sunday 10:00am-9:00pm"
+//   printedDate[3] = "Sunday 10:00am-9:00pm"
+//   printedDate[4] = "Sunday 10:00am-9:00pm"
+//   printedDate[5] = "Sunday 10:00am-9:00pm"
+//   printedDate[6] = "Sunday 10:00am-9:00pm"
+//   const stringDate = weekday[date.getDay()];
+//   document.getElementById("realDate").innerHTML = printedDate;
+// }
